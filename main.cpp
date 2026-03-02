755273d242ea1dab576a9a74d04a67698d5a9b7d
theJuniorProgrammer3 <175095475+theJuniorProgrammer3@users.noreply.github.com>
2026-03-02 23:09:15 +0700

    Prototype #2

--- main.cpp
+++ main.cpp
@@ -1,33 +1,42 @@
 #include <ncurses.h>
 #include <vector>
+#include <random>
 
 using namespace std;
 
-	vector<vector<bool>> world = {
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-	};
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<unsigned int> dis(0, 2);
+
+vector<vector<string>> world = {
+	{
+"####################",
+"#                  #",
+"#                  #",
+"#        &         #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"################ ###"
+	}
+};
 
 unsigned int minmax(int i) {
-	if(i > 9) return 9;
+	if(i > 19) return 19;
 	if(i < 0) return 0;
 	return i;
 }
-pair<unsigned int, unsigned int> pos = {4, 5};
-void changePos(int up, int right) {
-	world[pos.first][pos.second] = 0;
-	pos.first = minmax(pos.first + up);
-	pos.second = minmax(pos.second + right);
-	world[pos.first][pos.second] = 1;
+vector<unsigned int> pPos = {0, 3, 9};
+
+void changePos(int ud, int rl) {
+	world[pPos[0]][pPos[1]][pPos[2]] = ' ';
+	world[pPos[0]][pPos[1] + ud][pPos[2]] == '#' ? : pPos[1] += ud;
+	world[pPos[0]][pPos[1]][pPos[2] + rl] == '#' ? : pPos[2] += rl;
+	world[pPos[0]][pPos[1]][pPos[2]] = '&';
 }
+
 
 int main() {
 	initscr();
@@ -36,9 +45,10 @@ int main() {
 	int inp;
 	while(true) {
 		refresh();
-		for(auto b : world) {
+		for(auto b : world[pPos[0]]) {
 			for(auto c : b) {
-				printw("[%s] ", (c == true ? "&" : " "));
+				//printw("[%c]", (c == "p" ? "&" : " ));
+				printw("%c", c);
 			}
 			printw("\n");
 		}
