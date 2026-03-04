993054dd90545723e8180103e54c73174e3331e3
theJuniorProgrammer3 <175095475+theJuniorProgrammer3@users.noreply.github.com>
2026-03-04 23:33:00 +0800

    Prototype #6: 'The block is sane now...' (Add more feature for move block mechanism, improve shelf feature, bug fixes)

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ random_device rd;
 mt19937 gen(rd());
 uniform_int_distribution<unsigned int> dis(0, 2);
 uniform_int_distribution<unsigned int> disX(1, 18);
+bernoulli_distribution disLuckInShelf(0.3);
+uniform_int_distribution<unsigned int> disInShelf(0, 10);
 uniform_int_distribution<unsigned int> disY(1, 8);
 uniform_int_distribution<unsigned int> disIs(1, 3);
 
@@ -40,7 +42,7 @@ vector<vector<string>> world = {
 	}
 };
 
-uint8_t lastDoor = 16;
+vector<uint8_t> doors = {16, 16};
 void addWorld() {
 	vector<string> w = {
 "####################",
@@ -54,23 +56,22 @@ void addWorld() {
 "#                  #",
 "####################"
 	};
-	w[0][lastDoor] = ' ';
-	for(int i = 0; i < 5; ++i) {
+	w[0][doors.back()] = ' ';
+	for(int i = 0; i < 3; ++i) {
 		w[disY(gen)][disX(gen)] = '=';
 	}
-	for(int i = 0; i < 3; ++i) {
 		w[disY(gen)][disX(gen)] = 'B';
-	}
-	lastDoor = disX(gen);
-	w[9][lastDoor] = ' ';
-	world.push_back(w);
+		w[disY(gen)][disX(gen)] = 'B';
+	doors.push_back(disX(gen));
+	w[9][doors.back()] = ' ';
+	world.push_back(w);	
 }
 
 vector<unsigned int> pPos = {0, 3, 9};
 
 void changePos(int ud, int rl) {
 	world[pPos[0]][pPos[1]][pPos[2]] = ' ';
-	if(pPos[1] + ud == world[pPos[0]].size()) {
+	if(pPos[1] + ud == 10) { // world Y size - 1 + 1
 		pPos[0]++;
 		pPos[1] = 0;
 		if(pPos[0] >= world.size()) {
@@ -79,7 +80,7 @@ void changePos(int ud, int rl) {
 	} else if((int)pPos[1] + ud == -1) {
 		if(pPos[0] > 0) {
 		pPos[0]--;
-		pPos[1] = world[pPos[0]].size() - 1;
+		pPos[1] = 9; // world Y size - 1
 		}
 	}
 	if(world[pPos[0]][pPos[1] + ud][pPos[2]] == ' ') pPos[1] += ud;
@@ -89,18 +90,41 @@ void changePos(int ud, int rl) {
 
 map<vector<unsigned int>, vector<int>> shelfList;
 
+string intToItem(int i) {
+	switch(i) {
+		case 0:
+			return "Empty";
+			break;
+		case 1:
+			return "Cash";
+			break;
+		case 2:
+			return "Atropine";
+			break;
+		default: // case 3, but no warning
+			return "Glucose";
+			break;
+	}
+}
+
 void openShelf(vector<unsigned int> pos) {
 	nodelay(stdscr, FALSE);
 	clear();
 	auto isi = shelfList[pos];
 	if(isi.empty()) {
-		for(int i = 0; i < 10; ++i) {
-			shelfList[pos].push_back(disIs(gen));
+		int itemCount = disInShelf(gen);
+		itemCount %= disLuckInShelf(gen) ? 10 : 4;
+		if(itemCount == 0) {
+			shelfList[pos].push_back(0);
+		} else {
+			for(int i = 0; i < itemCount; ++i) {
+				shelfList[pos].push_back(disIs(gen));
+			}
 		}
 		isi = shelfList[pos];
 	}
 	for(auto i : isi) {
-		printw("%i ", i);
+		printw("%s ", intToItem(i).c_str());
 	}
 	refresh();
 	getch();
@@ -136,10 +160,18 @@ void push(char type) {
 	char itm;
 	pos = {pPos[0], pPos[1] + direction[type].first, pPos[2] + direction[type].second};
 	itm = world[pos[0]][pos[1]][pos[2]];
-	world[pos[0]][pos[1]][pos[2]] = ' ';
-	switch(itm) {
-		case 'B':
-			world[pos[0]][pos[1] + direction[type].first][pos[2] + direction[type].second] = 'B';
+	if(itm == 'B') {
+			if(pos[1] == 9) { // world pos 0 size - 1
+				world[pos[0]][9][pos[2]] = ' ';
+				if(pos[0] + 1 >= world.size()) addWorld();
+				world[pos[0] + 1][1][pos[2]] = 'B';
+			} else if(pos[1] == 0) {
+				world[pos[0]][0][pos[2]] = ' ';
+				world[pos[0] - 1][8][pos[2]] = 'B';
+			} else if(world[pos[0]][pos[1] + direction[type].first][pos[2] + direction[type].second] == ' ') {
+			world[pos[0]][pos[1]][pos[2]] = ' ';
+				world[pos[0]][pos[1] + direction[type].first][pos[2] + direction[type].second] = itm;
+			}
 
 	}
 }
