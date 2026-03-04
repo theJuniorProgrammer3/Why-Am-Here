db8dedf2d106f2147fc18d14433b46e1fecb07fb
theJuniorProgrammer3 <175095475+theJuniorProgrammer3@users.noreply.github.com>
2026-03-04 20:29:53 +0800

    Prototype #5: 'Move the block\!' (Add block move blocks mechanics (buggy/early), add infinite world generator)'

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string>
 #include <vector>
 #include <random>
 #include <map>
@@ -8,6 +9,8 @@ using namespace std;
 random_device rd;
 mt19937 gen(rd());
 uniform_int_distribution<unsigned int> dis(0, 2);
+uniform_int_distribution<unsigned int> disX(1, 18);
+uniform_int_distribution<unsigned int> disY(1, 8);
 uniform_int_distribution<unsigned int> disIs(1, 3);
 
 vector<vector<string>> world = {
@@ -17,7 +20,7 @@ vector<vector<string>> world = {
 "#             =    #",
 "#        &         #",
 "#                  #",
-"#                  #",
+"#         B        #",
 "#   =              #",
 "#              =   #",
 "#                  #",
@@ -27,7 +30,7 @@ vector<vector<string>> world = {
 "################ ###",
 "#                  #",
 "# =                #",
-"#                  #",
+"#           B      #",
 "#       =          #",
 "#                  #",
 "#                  #",
@@ -37,14 +40,41 @@ vector<vector<string>> world = {
 	}
 };
 
+uint8_t lastDoor = 16;
+void addWorld() {
+	vector<string> w = {
+"####################",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"####################"
+	};
+	w[0][lastDoor] = ' ';
+	for(int i = 0; i < 5; ++i) {
+		w[disY(gen)][disX(gen)] = '=';
+	}
+	for(int i = 0; i < 3; ++i) {
+		w[disY(gen)][disX(gen)] = 'B';
+	}
+	lastDoor = disX(gen);
+	w[9][lastDoor] = ' ';
+	world.push_back(w);
+}
+
 vector<unsigned int> pPos = {0, 3, 9};
 
 void changePos(int ud, int rl) {
 	world[pPos[0]][pPos[1]][pPos[2]] = ' ';
 	if(pPos[1] + ud == world[pPos[0]].size()) {
-		if(pPos[0] + 1 < world.size()) {
 		pPos[0]++;
 		pPos[1] = 0;
+		if(pPos[0] >= world.size()) {
+			addWorld();
 		}
 	} else if((int)pPos[1] + ud == -1) {
 		if(pPos[0] > 0) {
@@ -77,29 +107,40 @@ void openShelf(vector<unsigned int> pos) {
 	nodelay(stdscr, TRUE);
 }
 
+map<char, pair<int, int>> direction = { // xhar, Y, X
+					// I will optimize later
+ {'e', {-1, 0}},
+ {'u', {-1, 0}},
+ {'s', {0, -1}},
+ {'h', {0, -1}},
+ {'d', {1, 0}},
+ {'j', {1, 0}},
+ {'f', {0, 1}},
+ {'k', {0, 1}},
+};
+
 void inter(char type) {
 	vector<unsigned int> pos;
 	char itm;
-		switch(type) {
-			case 'e':
-				pos = {pPos[0], pPos[1] - 1, pPos[2]};
-				break;
-			case 's':
-				pos = {pPos[0], pPos[1], pPos[2] - 1};
-				break;
-			case 'd':
-				pos = {pPos[0], pPos[1] + 1, pPos[2]};
-				break;
-			case 'f':
-				pos = {pPos[0], pPos[1], pPos[2] + 1};
-				break;
-				
-		}
+	pos = {pPos[0], pPos[1] + direction[type].first, pPos[2] + direction[type].second};
 	itm = world[pos[0]][pos[1]][pos[2]];
 	switch(itm) {
 		case '=':
 			openShelf(pos);
 			break;
+	}
+}
+
+void push(char type) {
+	vector<unsigned int> pos;
+	char itm;
+	pos = {pPos[0], pPos[1] + direction[type].first, pPos[2] + direction[type].second};
+	itm = world[pos[0]][pos[1]][pos[2]];
+	world[pos[0]][pos[1]][pos[2]] = ' ';
+	switch(itm) {
+		case 'B':
+			world[pos[0]][pos[1] + direction[type].first][pos[2] + direction[type].second] = 'B';
+
 	}
 }
 
@@ -127,6 +168,7 @@ int main() {
 		else if(inp == KEY_DOWN) changePos(1, 0);
 		else if(inp == 'q') break;
 		if(inp == 'e' || inp == 's' || inp == 'd' || inp == 'f') inter(inp);
+		if(inp == 'u' || inp == 'h' || inp == 'j' || inp == 'k') push(inp);
 		napms(17);
 		clear();
 	}
